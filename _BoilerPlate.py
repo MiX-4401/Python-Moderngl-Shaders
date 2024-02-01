ca73783eae22d34b1ad383f03192f357b9733c64
Ethan Radford <ej.radford@outlook.com.au>
2024-02-01 15:34:18 +1100

    update var names

--- _BoilerPlate.py
+++ _BoilerPlate.py
@@ -20,7 +20,7 @@ uniform sampler2D myTexture;
 in vec2 uvs;
 out vec4 fColour;
 
-void main(){
+void main(){d_update
     vec4 colour = texture(myTexture, uvs).rgba;
 
     fColour = vec4(colour.rgb, colour.a);
@@ -32,14 +32,14 @@ void main(){
 
         self.load_program()
 
-         # Load render target texture
-        self.new_texture: mgl.Texture     = self.ctx.texture(size=self.my_texture.size, components=4)
-        self.new_texture.filter: tuple    = (mgl.NEAREST, mgl.NEAREST)
-        self.framebuffer: mgl.Framebuffer = self.ctx.framebuffer(color_attachments=[self.new_texture])
+        # Load render target texture
+        self.final_texture: mgl.Texture     = self.ctx.texture(size=self.start_texture.size, components=4)
+        self.final_texture.filter: tuple    = (mgl.NEAREST, mgl.NEAREST)
+        self.framebuffer: mgl.Framebuffer   = self.ctx.framebuffer(color_attachments=[self.final_texture])
 
         # Create shader program
-        self.new_program: mgl.Program     = self.ctx.program(vertex_shader=Main.main_vertex, fragment_shader=ShaderProgram.program_frag)
-        self.new_vao:     mgl.VertexArray = self.ctx.vertex_array(self.new_program, [(self.quad_buffer, "2f 2f", "aPosition", "aTexCoord")])
+        self.my_program: mgl.Program     = self.ctx.program(vertex_shader=Main.main_vertex, fragment_shader=ShaderProgram.program_frag)
+        self.my_vao:     mgl.VertexArray = self.ctx.vertex_array(self.my_program, [(self.quad_buffer, "2f 2f", "aPosition", "aTexCoord")])
 
     @Main.d_update
     def update(self):
@@ -47,18 +47,19 @@ void main(){
 
     def garbage_cleanup(self):
         super().garbage_cleanup() # A better way then using decorators in this context
-        self.new_program.release()
-        self.new_program.release()
+        self.my_program.release()
+        self.my_program.release()
         self.framebuffer.release()
-        self.new_vao.release()
+        self.my_vao.release()
         self.new_texture.release()
 
     @Main.d_draw
     def draw(self):
+
         self.framebuffer.use()
-        self.my_texture.use(location=0)
-        self.new_program["myTexture"] = 0
-        self.new_vao.render(mgl.TRIANGLE_STRIP)
+        self.start_texture.use(location=0)
+        self.my_program["myTexture"] = 0
+        self.my_vao.render(mgl.TRIANGLE_STRIP)
 
         self.ctx.screen.clear(red=0.0, green=0.0, blue=0.0, alpha=1.0)
         self.ctx.screen.use()
