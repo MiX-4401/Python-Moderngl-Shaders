9bdb8733e2dbd2e15b793a330025781272a99cb0
Ethan Radford <ej.radford@outlook.com.au>
2024-02-03 00:24:01 +1100

    Created gaussian blur shader

---
+++ _shaderPasses/gaussianBlur.py
@@ -0,0 +1,46 @@
+
+from _shaderPasses._lib import ShaderPass
+import moderngl as mgl
+
+
+class GaussianBlur(ShaderPass):
+    def __init__(self, ctx:mgl.Context, size:tuple, components:int=4):
+        super().__init__(ctx=ctx)
+
+        self.load_shaders(paths=(
+            r"_shaderPasses\__gaussian_blur.frag",
+        ))
+
+
+        self.create_program(name="gaussianBlur", vert=self.shaders["vert"]["__base_vert"], frag=self.shaders["frag"]["__gaussian_blur"])
+        self.create_vao(name="gaussianBlur", program=self.programs["gaussianBlur"], buffer=self.buffers["base"], args=["2f 2f", "bPos", "bTexCoord"])
+
+        self.create_texture(name="ping", size=size, components=components, filter=(mgl.NEAREST, mgl.NEAREST))
+        self.create_texture(name="pong", size=size, components=components, filter=(mgl.NEAREST, mgl.NEAREST))
+        self.create_framebuffer(name="ping", attachments=[self.textures["ping"]])
+        self.create_framebuffer(name="pong", attachments=[self.textures["pong"]])
+
+    def run(self, texture:mgl.Texture, output:mgl.Framebuffer, x:int=1, **uniforms):
+
+        """
+            2-Pass Gaussian Blur Shader Pass
+            Using ping-pong framebuffers/textures to create blur on the xy axies
+        """
+
+        # Initial sample
+        self.textures["pong"].write(texture.read())
+
+        for x in range(x):
+            self.sample_framebuffer(framebuffer="pong")
+            self.render_direct(program="gaussianBlur", vao="gaussianBlur", framebuffer=self.framebuffers["ping"], uTexture=0, horizontal=True)
+
+            # Rebound sample 
+            self.sample_framebuffer(framebuffer="ping")
+            self.render_direct(program="gaussianBlur", vao="gaussianBlur", framebuffer=self.framebuffers["pong"], uTexture=0, horizontal=False)
+
+        # Write to output
+        output.write(self.framebuffers["pong"].color_attachments[0].read())
+
+
+
+
