f157f6d2c70e45524e95aa70d22f5d63dfffeb30
Ethan Radford <ej.radford@outlook.com.au>
2024-02-03 00:31:26 +1100

    made updates

--- _shaderPasses/colourInvert.py
+++ _shaderPasses/colourInvert.py
@@ -13,6 +13,12 @@ class ColourInvert(ShaderPass):
 
         self.create_program(name="invert", vert=self.shaders["vert"]["__base_vert"], frag=self.shaders["frag"]["__colour_invert"])
         self.create_vao(name="invert", program=self.programs["invert"], buffer=self.buffers["base"], args=["2f 2f", "bPos", "bTexCoord"])
+        self.create_framebuffer(name="invertColour")
 
-    def run(self, framebuffer:mgl.Framebuffer, **uniforms):
-        return self.render_direct(program="invert", vao="invert", framebuffer=framebuffer)
+    def run(self, texture:mgl.Texture, output:mgl.Framebuffer, **uniforms):
+        
+        # Render
+        self.render_direct(program="invert", vao="invert", framebuffer=self.framebuffers["invertColour"])
+        
+        # Write to output
+        output.write(self.framebuffers["invertColour"].color_attachments[0].read())
