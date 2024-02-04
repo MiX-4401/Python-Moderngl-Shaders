13b9ca58489d18980d9372a00382dd1eafad7412
Ethan Radford <ej.radford@outlook.com.au>
2024-02-04 15:58:48 +1100

    deleted

--- a.py
+++
@@ -1,4 +0,0 @@
-def func(program, buffer, args):
-    print(fr"self.ctx.buffer({program}, [({buffer}, {', '.join(args)})])")
-
-func(program="myP", buffer="quad", args=["2f 2f", "bPos", "bTexCoord"])
